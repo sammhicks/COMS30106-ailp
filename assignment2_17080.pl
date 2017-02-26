73ca9cfeb394cb469695f1476fd47dec8f871225
Samuel Hicks <sh14820@my.bristol.ac.uk>
2017-02-26 22:51:37 +0000

    Implemented cached previous positions
    We create a map of previously visited locations, and their corresponding lowest costs.

--- assignment2_17080.pl
+++ assignment2_17080.pl
@@ -13,12 +13,15 @@ solve_task_1_3(Task, [cost(Final_Cost), depth(Final_Depth)]) :-
   Start_Cost = 0,
   Start_Depth = 0,
   calculate_heuristic(Task, Start_Position, Start_Heuristic),
+  Start_Path = path(Start_Cost, Start_Depth, Start_Heuristic, [Start_Position]),
+  insert_visited_position(Start_Position, Start_Cost, costs{}, Start_Costs),
   solve_task_bfs(
       Task,
-      [path(Start_Cost, Start_Depth, Start_Heuristic, [Start_Position])],
+      Start_Costs,
+      [Start_Path],
       path(Final_Cost, Final_Depth, _Final_Heuristic, Final_Reverse_Path)
   ),
-  !, % Once we've found a solution, we know that it's the best one
+  !, % Once we've found a solution, we hope that it's the best one
   reverse(Final_Reverse_Path, [_Start_Position|Final_Path]),
   agent_do_moves(oscar, Final_Path),
   !. % we're done, prune any remaing choice points
@@ -34,39 +37,20 @@ solve_task_4(Task,Cost):-
 
 
 %%%%%%%%%% Useful predicates %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% backtracking depth-first search, needs to be changed to agenda-based A*
-% solver_task_bt(+Task, +Current_Path, +Current_Depth, -Reverse_Path,
-% -Costs, -New_Position)
-solve_task_bt(Task, Current_Path, Current_Depth, Reverse_Path, [cost(Total_Cost), depth(Current_Depth)], New_Position) :-
-  achieved(Task, Current_Path, Reverse_Path, Total_Cost, New_Position),
-  !. % If we've achieved our task, stop.
-
-solve_task_bt(Task, Current_Path, Current_Depth, Final_Reverse_Path, Total_Cost, New_Position) :-
-  Current_Path = [c(Current_Cost, Current_Position)|Reverse_Path],
-  search_bt(Task, Current_Position, Adjacent_Position, Adjacent_Position, Move_Cost), % Search for a suitable adjacent position
-  \+ memberchk(Adjacent_Position, Reverse_Path),  % check we have not been here already
-  % Calculate the new state
-  New_Depth is Current_Depth + 1,
-  New_Cost is Current_Cost + Move_Cost,
-  New_Path = [c(New_Cost, Adjacent_Position), Adjacent_Position|Reverse_Path],
-  % Solve using the new state
-  solve_task_bt(Task, New_Path, New_Depth, Final_Reverse_Path, Total_Cost, New_Position).
-
-
 % solve_task_bfs(+Task, +Current_Paths, -Final_Path)
-solve_task_bfs(Task, [Current_Path|_Rest], Current_Path) :-
+solve_task_bfs(Task, _Visited_Positions, [Current_Path|_Rest], Current_Path) :-
   achieved(Task, Current_Path).
 
-
-solve_task_bfs(Task, [path(Current_Cost, Current_Depth, _Current_Heuristic, Current_Path)|Rest], Final_Path) :-
+solve_task_bfs(Task, Current_Visited_Positions, [path(Current_Cost, Current_Depth, _Current_Heuristic, Current_Path)|Rest], Final_Path) :-
   Current_Path = [Current_Position|_],
   empty_map_adjacent(Current_Position, Adjacent_Positions),
-  filter_positions(Current_Path, Adjacent_Positions, Filtered_Adjacent_Positions),
-  prepend_to_paths(Current_Path, Filtered_Adjacent_Positions, New_Paths),
+  prepend_to_paths(Current_Path, Adjacent_Positions, New_Paths),
   construct_paths(Task, Current_Cost, Current_Depth, New_Paths, New_Path_Functors),
-  append(Rest, New_Path_Functors, New_Agenda),
+  filter_positions(Current_Visited_Positions, New_Path_Functors, Filtered_New_Path_Functors),
+  insert_previous_paths(Filtered_New_Path_Functors, Current_Visited_Positions, New_Visited_Positions),
+  append(Rest, Filtered_New_Path_Functors, New_Agenda),
   sort(3, @=<, New_Agenda, Sorted_New_Agenda),
-  solve_task_bfs(Task, Sorted_New_Agenda, Final_Path).
+  solve_task_bfs(Task, New_Visited_Positions, Sorted_New_Agenda, Final_Path).
 
 
 % achieved(+Task, +Current_Path)
@@ -81,15 +65,6 @@ achieved(go(Target), path(_Final_Cost, _Final_Depth, _Final_Heuristic, [Target|_
 %  ( O=none    -> true
 %  ; otherwise -> RPath = [Last|_],map_adjacent(Last,_,O)
 %  ).
-
-% search(+Target_Position, +Current_Position, -Adjacent_Position,
-% -Adjacent_Position, -Cost)
-search_bt(go(Target_Position), Current_Position, Adjacent_Position, Adjacent_Position, 1) :-
-  empty_map_adjacent(Current_Position, Adjacent_Positions), % Find all empty adjacent cells
-  position_distances(Target_Position, Adjacent_Positions, Position_Distances), % Calculate their distances from the target
-  sort(2, @=<, Position_Distances, Sorted_Position_Distances), % Sort them based on their distances
-  member(pd(Adjacent_Position, _Distance), Sorted_Position_Distances). % Return the cells, starting with the smallest
-
 
 % empty_map_adjacent(+Current_Position, -All_Empty_Adjacent_Positions)
 empty_map_adjacent(Current_Position, All_Empty_Adjacent_Positions) :-
@@ -106,15 +81,21 @@ empty_map_adjacent_acc(Current_Position, Current_Adjacent_Positions, All_Adjacen
 empty_map_adjacent_acc(_Current_Position, All_Adjacent_Positions, All_Adjacent_Positions).
 
 
-% filter_positions(+Disallowed_Positions, +Positions, -All_Filtered_Positions)
-filter_positions(_Disallowed_Positions, [], []).
+% filter_positions(+Visited_Positions, +Positions,
+% -All_Filtered_Positions)
+filter_positions(_Visited_Positions, [], []).
 
-filter_positions(Disallowed_Positions, [Position|Positions], All_Filtered_Positions) :-
-  (   memberchk(Position, Disallowed_Positions)
-  ->  All_Filtered_Positions = Filtered_Positions
-  ;   All_Filtered_Positions = [Position|Filtered_Positions]
+filter_positions(Visited_Positions, [Path|Paths], All_Filtered_Paths) :-
+  (   better_path_found(Visited_Positions, Path)
+  ->  All_Filtered_Paths = Filtered_Paths
+  ;   All_Filtered_Paths = [Path|Filtered_Paths]
   ),
-  filter_positions(Disallowed_Positions, Positions, Filtered_Positions).
+  filter_positions(Visited_Positions, Paths, Filtered_Paths).
+
+
+better_path_found(Visited_Positions, path(Cost, _Depth, _Heuristic, [Position|_])) :-
+  contains_visited_position(Visited_Positions, Position, Visited_Cost),
+  Visited_Cost =< Cost.
 
 
 % prepend_to_paths(+Previous_Path, +New_End_Positions, -New_Paths)
@@ -139,6 +120,27 @@ construct_path(Task, Current_Cost, Current_Depth, Path, path(New_Cost, New_Depth
 
 calculate_heuristic(go(Target), Current_Position, Heuristic) :-
   map_distance(Target, Current_Position, Heuristic).
+
+
+contains_visited_position(Existing_Costs, p(X, Y), Cost) :-
+  get_dict(Y, Existing_Costs, Existing_Row),
+  get_dict(X, Existing_Row, Cost).
+
+
+insert_previous_paths([], Existing_Costs, Existing_Costs).
+
+insert_previous_paths([path(Cost, _Depth, _Heuristic, [Position|_])|Paths], Old_Existing_Costs, New_Existing_Costs) :-
+  insert_visited_position(Position, Cost, Old_Existing_Costs, Current_Existing_Costs),
+  insert_previous_paths(Paths, Current_Existing_Costs, New_Existing_Costs).
+
+
+insert_visited_position(p(X, Y), Cost, Existing_Costs, New_Costs) :-
+  (   get_dict(Y, Existing_Costs, Existing_Row)
+  ->  true
+  ;   Existing_Row = costs{}
+  ),
+  put_dict(X, Existing_Row, Cost, New_Row),
+  put_dict(Y, Existing_Costs, New_Row, New_Costs).
 
 scan_map :-
   between(1, 20, Y),
